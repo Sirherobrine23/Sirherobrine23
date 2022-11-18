42575500f1720fa6977befc32ab5959ec61e6264
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-18 22:45:10 +0000

    Update files

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sirherobrine23</title>
+        <meta name="description" content="Sirherobrine23" />
+      </Head>
+      <div>
+        <h2>Quem é Sirherobrine23</h2>
+        <div>
+          Sou Matheus Sampaio Queiroga, desenvolvedor de Back-end com Javascript (Node.js), e basico de Front-end (react)
+        </div>
+      </div>
+    </>
+  )
+}
