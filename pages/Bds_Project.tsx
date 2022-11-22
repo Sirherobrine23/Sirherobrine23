81a7c7d0f6712da1cc22e8b03f63b106c6d9e272
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-22 20:35:55 -0300

    Add bds project page

---
+++ pages/Bds_Project.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>The Bds Maneger Project</title>
+        <meta name="description" content="Sirherobrine23" />
+      </Head>
+      <div>
+        <h2>The Bds Maneger</h2>
+        <div>
+          O The Bds Maneger surgiu quando eu estava precisando gerenciar meu servidor de minecraft bedrock, e o primeiros criei um script shell na qual gerenciava o meu servidor no Linux mas eu não estava contente pois eu não só usava o linux.
+          <br/>
+          Logo em sequida veio um script para powershell mas foi para o brejo e apagado do Github.
+          <br/>
+          entre 2017/2018 estava aprendendo javascript e o Bds Maneger foi minha primeira cobaia, essa versão foi totalmente feita para executar scripts shell, então imaga como demorava para fazer qualquer coisa pois ele foi feito para o Electron
+        </div>
+      </div>
+    </>
+  )
+}
